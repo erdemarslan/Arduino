262e1b59827ad05ac6ccf0556cae538942c5602f
Erdem Arslan <erdemarslan@users.noreply.github.com>
2018-03-11 15:38:32 +0300

    Şarj Makinesi
    Ekran Sorunu Çözümü
    Bazı iyileştirmeler

--- Örnekler/Para Makinesi/Sarj_Otomati_4_Kanal/Sarj_Otomati_4_Kanal.ino
+++ Örnekler/Para Makinesi/Sarj_Otomati_4_Kanal/Sarj_Otomati_4_Kanal.ino
@@ -29,11 +29,10 @@ const byte relayPin_4 = 10;
 volatile double pulseKacPara = 1.00; // bu makineden para atıldığında gelen 1 pulsenin kaç liraya denk geleceğini ifade eder.
 double sarjdanDusulecekPara = 1.00; // butona bastığında kaç para düşelim bakiyeden!
 volatile double lira = 0.00;
-volatile bool paraGeldimi = false; // ekranda gelen parayı ifade etmek için kullanılacak! bu değere göre ekranda para geldiğinde değişiklik olacak!
-
 
 // Ekran Modu ile ilgili tanımlamalar!
 byte ekranModu = 0; // 0 - Ana Menü | 1 - Şarj Menüsü
+byte oncekiEkranModu = 1;
 volatile bool modGosterildimi = false;
 unsigned int ekranDonguSuresi = 5000; // 5 saniye!
 unsigned long sonEkranDegisimi = 0;
@@ -48,6 +47,7 @@ bool role_3_Acikmi = false;
 bool role_4_Acikmi = false;
 
 unsigned long roleAcikKalmaSuresi = 3600000; // 1 saat milisaniye cinsinden!
+//unsigned long roleAcikKalmaSuresi = 10000;
 
 unsigned long role_1_bitmeSuresi = 0;
 unsigned long role_2_bitmeSuresi = 0;
@@ -58,13 +58,15 @@ unsigned long role_4_bitmeSuresi = 0;
 // Para geldiğinde interrupt ile çalışacak fonksiyon
 void paraGeldi() {
   lira = lira + pulseKacPara; // o anki bakiyeye para ekliyoruz!
+  ekranModu = 1;
   modGosterildimi = false;
-  if(!paraGeldimi) {
-    paraGeldimi = true;
-  }
+  sonEkranDegisimi = millis() - ekranDonguSuresi - 1000; // her halükarda ekranın değişmesini sağlar!
 }
 
 void setup() {
+
+  Serial.begin(9600);
+  
   pinMode(ledPin_1, OUTPUT);
   pinMode(ledPin_2, OUTPUT);
   pinMode(ledPin_3, OUTPUT);
@@ -72,10 +74,10 @@ void setup() {
 
   // Buttonlar elektriksel olarak Pullup yapılmalı. 10K direnç kullanın
   // Yazılımsal olarak da pullup yapılabilir ama kararlı çalışmayabilir. tercih meselesi.
-  pinMode(buttonPin_1, INPUT);
-  pinMode(buttonPin_2, INPUT);
-  pinMode(buttonPin_3, INPUT);
-  pinMode(buttonPin_4, INPUT);
+  pinMode(buttonPin_1, INPUT_PULLUP);
+  pinMode(buttonPin_2, INPUT_PULLUP);
+  pinMode(buttonPin_3, INPUT_PULLUP);
+  pinMode(buttonPin_4, INPUT_PULLUP);
 
   
   pinMode(relayPin_1, OUTPUT);
@@ -107,208 +109,51 @@ void loop() {
   // Açık röle varsa zamanlarını kontrol et!
   if(sarjVarmi) {
     roleKontrol();
-  }
 
-  // Ekran modu sadece şarj varken değişsin. Röleler kapalıyken sadece ana menü gösterilsin
-  if(sarjVarmi) {
     if(millis() - sonEkranDegisimi > ekranDonguSuresi) {
-      ekranModu = ekranModu == 0 ? 1 : 0;
-      modGosterildimi = false;
-      //sonSaniyeGosterimi = millis();
+      if(ekranModu == 0) {
+        ekranModu = 1;
+        sonEkranDegisimi = millis();
+      } else {
+        ekranModu = 0;
+        modGosterildimi = false;
+        sonEkranDegisimi = millis();
+      }
+    }
+  } else {
+    if(ekranModu == 1) {
+        ekranModu = 0;
+        modGosterildimi = false;
+        sonEkranDegisimi = millis();
     }
   }
+  
   EkranGoster();
 }
 
-void buttonKontrol() {
-  byte button_deger_1 = digitalRead(buttonPin_1);
-  byte button_deger_2 = digitalRead(buttonPin_2);
-  byte button_deger_3 = digitalRead(buttonPin_3);
-  byte button_deger_4 = digitalRead(buttonPin_4);
-
-  if(button_deger_1 == HIGH) {
-    roleCalistir(1);
-    return; // looptan burada çıkar ve loopun başına döner! böylelikle butonlara aynı anda basılsa bile ilk sıradaki çalışır.
-  }
-
-  if(button_deger_2 == HIGH) {
-    roleCalistir(2);
-    return;
-  }
-
-  if(button_deger_3 == HIGH) {
-    roleCalistir(3);
-    return;
-  }
-
-  if(button_deger_4 == HIGH) {
-    roleCalistir(4);
-    return;
-  }
-}
-
-void roleCalistir(byte roleNo) {
-  sarjVarmi = true;
-
-  if(roleNo == 1) {
-    if(role_1_Acikmi) {
-      // röle zaten açıksa süre ekle!
-      role_1_bitmeSuresi += roleAcikKalmaSuresi;
-    } else {
-      // röle açık değilse bitme süresini ayarla. ve çalıştır!
-      role_1_bitmeSuresi = millis() + roleAcikKalmaSuresi;
-      // ledi aktifleştir!
-      digitalWrite(ledPin_1, HIGH);
-      // röleyi aç!
-      digitalWrite(relayPin_1, HIGH);
-      // rölenin açık olduğunu kaydet!
-      role_1_Acikmi = true;
-    }
-  }
-
-  if(roleNo == 2) {
-    if(role_2_Acikmi) {
-      // röle zaten açıksa süre ekle!
-      role_2_bitmeSuresi += roleAcikKalmaSuresi;
-    } else {
-      // röle açık değilse bitme süresini ayarla. ve çalıştır!
-      role_2_bitmeSuresi = millis() + roleAcikKalmaSuresi;
-      // ledi aktifleştir!
-      digitalWrite(ledPin_2, HIGH);
-      // röleyi aç!
-      digitalWrite(relayPin_2, HIGH);
-      // rölenin açık olduğunu kaydet!
-      role_2_Acikmi = true;
-    }
-  }
-
-  if(roleNo == 3) {
-    if(role_3_Acikmi) {
-      // röle zaten açıksa süre ekle!
-      role_3_bitmeSuresi += roleAcikKalmaSuresi;
-    } else {
-      // röle açık değilse bitme süresini ayarla. ve çalıştır!
-      role_3_bitmeSuresi = millis() + roleAcikKalmaSuresi;
-      // ledi aktifleştir!
-      digitalWrite(ledPin_3, HIGH);
-      // röleyi aç!
-      digitalWrite(relayPin_3, HIGH);
-      // rölenin açık olduğunu kaydet!
-      role_3_Acikmi = true;
-    }
-  }
-
-  if(roleNo == 4) {
-    if(role_4_Acikmi) {
-      // röle zaten açıksa süre ekle!
-      role_4_bitmeSuresi += roleAcikKalmaSuresi;
-    } else {
-      // röle açık değilse bitme süresini ayarla. ve çalıştır!
-      role_4_bitmeSuresi = millis() + roleAcikKalmaSuresi;
-      // ledi aktifleştir!
-      digitalWrite(ledPin_4, HIGH);
-      // röleyi aç!
-      digitalWrite(relayPin_4, HIGH);
-      // rölenin açık olduğunu kaydet!
-      role_4_Acikmi = true;
-    }
-  }
-
-  lira -= sarjdanDusulecekPara;
-}
-
-void roleKontrol() {
-  unsigned long simdi = millis();
-  bool sarj_1_kapat = false;
-  bool sarj_2_kapat = false;
-  bool sarj_3_kapat = false;
-  bool sarj_4_kapat = false;
-  // sadece röle açıksa işlem yap!
-
-  //1. röle
-  if(role_1_Acikmi) {
-    if(role_1_bitmeSuresi - simdi < 0) {
-      sarj_1_kapat = true; // en son hepsini kontrol edeceğiz ona göre şarjıkapatalımmi yi değiştireceğiz!
-      digitalWrite(ledPin_1, LOW); // ledi kapat!
-      digitalWrite(relayPin_1, LOW); // röleyi kapat!
-      role_1_bitmeSuresi = 0; // sayacı 0 la ne olur ne olmaz!
-      role_1_Acikmi = false;
-    }
-  } else {
-    sarj_1_kapat = true;
-  }
-
-
-  if(role_2_Acikmi) {
-    if(role_2_bitmeSuresi - simdi < 0) {
-      sarj_2_kapat = true; // en son hepsini kontrol edeceğiz ona göre şarjıkapatalımmi yi değiştireceğiz!
-      digitalWrite(ledPin_2, LOW); // ledi kapat!
-      digitalWrite(relayPin_2, LOW); // röleyi kapat!
-      role_2_bitmeSuresi = 0; // sayacı 0 la ne olur ne olmaz!
-      role_2_Acikmi = false;
-    }
-  } else {
-    sarj_2_kapat = true;
-  }
-
-
-  if(role_3_Acikmi) {
-    if(role_3_bitmeSuresi - simdi < 0) {
-      sarj_3_kapat = true; // en son hepsini kontrol edeceğiz ona göre şarjıkapatalımmi yi değiştireceğiz!
-      digitalWrite(ledPin_3, LOW); // ledi kapat!
-      digitalWrite(relayPin_3, LOW); // röleyi kapat!
-      role_3_bitmeSuresi = 0; // sayacı 0 la ne olur ne olmaz!
-      role_3_Acikmi = false;
-    }
-  } else {
-    sarj_3_kapat = true;
-  }
-
-
-  if(role_4_Acikmi) {
-    if(role_4_bitmeSuresi - simdi < 0) {
-      sarj_4_kapat = true; // en son hepsini kontrol edeceğiz ona göre şarjıkapatalımmi yi değiştireceğiz!
-      digitalWrite(ledPin_4, LOW); // ledi kapat!
-      digitalWrite(relayPin_4, LOW); // röleyi kapat!
-      role_4_bitmeSuresi = 0; // sayacı 0 la ne olur ne olmaz!
-      role_4_Acikmi = false;
-    }
-  } else {
-    sarj_4_kapat = true;
-  }
-
-  // tüm röleler kapanıyor mu?
-  if(sarj_1_kapat == true && sarj_2_kapat == true && sarj_3_kapat == true && sarj_4_kapat == true) {
-    sarjVarmi = false;
-    // ekranı anamenüye alalım ne olur ne olmaz!
-    ekranModu = 0;
-    sonEkranDegisimi = millis() - ekranDonguSuresi - 1000; // her halükarda ekranın değişmesini sağlar!
-  }
-}
-
-
 void EkranGoster() {
-  if(ekranModu == 0 && modGosterildimi == false) {
-    lcd.clear();
-    lcd.home();
-    lcd.print(" LUTFEN PARA ATINIZ");
-    lcd.setCursor(0,1); // 2. satır!
-    String bakiye = "BAKIYE: " + String(lira) + " TL";
-    lcd.print(bakiye);
-    lcd.setCursor(0,2); // 3. satır
-    lcd.print("R1    R2    R3    R4");
-    lcd.setCursor(0,3); // 4. satır!
-    String r1d = role_1_Acikmi == true ? "A" : "K";
-    String r2d = role_2_Acikmi == true ? "A" : "K";
-    String r3d = role_3_Acikmi == true ? "A" : "K";
-    String r4d = role_4_Acikmi == true ? "A" : "K";
-    lcd.print(r1d + "     " + r2d + "     " + r3d + "     " + r4d);
-    modGosterildimi = true;
-    sonEkranDegisimi = millis();
+  if(ekranModu == 0) {
+    if(modGosterildimi == false) {
+      lcd.clear();
+      lcd.home();
+      lcd.print(" LUTFEN PARA ATINIZ");
+      lcd.setCursor(0,1); // 2. satır!
+      String bakiye = "BAKIYE: " + String(lira) + " TL";
+      lcd.print(bakiye);
+      lcd.setCursor(0,2); // 3. satır
+      lcd.print("R1    R2    R3    R4");
+      lcd.setCursor(0,3); // 4. satır!
+      String r1d = role_1_Acikmi == true ? "A" : "K";
+      String r2d = role_2_Acikmi == true ? "A" : "K";
+      String r3d = role_3_Acikmi == true ? "A" : "K";
+      String r4d = role_4_Acikmi == true ? "A" : "K";
+      lcd.print(r1d + "     " + r2d + "     " + r3d + "     " + r4d);
+      modGosterildimi = true;
+      sonEkranDegisimi = millis();
+    }
   }
 
   if(ekranModu == 1) {
-    
     if(millis() - sonSaniyeGosterimi > 1000) {
       lcd.clear();
       long kalansure1 = role_1_Acikmi == true ? role_1_bitmeSuresi - millis() : 0;
@@ -387,3 +232,181 @@ void EkranGoster() {
   }
 }
 
+
+void roleKontrol() {
+  unsigned long simdi = millis();
+  bool sarj_1_kapat = false;
+  bool sarj_2_kapat = false;
+  bool sarj_3_kapat = false;
+  bool sarj_4_kapat = false;
+  // sadece röle açıksa işlem yap!
+
+  //1. röle
+  if(role_1_Acikmi) {
+    if(role_1_bitmeSuresi < simdi) {
+      sarj_1_kapat = true; // en son hepsini kontrol edeceğiz ona göre şarjıkapatalımmi yi değiştireceğiz!
+      digitalWrite(ledPin_1, LOW); // ledi kapat!
+      digitalWrite(relayPin_1, LOW); // röleyi kapat!
+      role_1_bitmeSuresi = 0; // sayacı 0 la ne olur ne olmaz!
+      role_1_Acikmi = false;
+    }
+  } else {
+    sarj_1_kapat = true;
+  }
+
+
+  if(role_2_Acikmi) {
+    if(role_2_bitmeSuresi < simdi) {
+      sarj_2_kapat = true; // en son hepsini kontrol edeceğiz ona göre şarjıkapatalımmi yi değiştireceğiz!
+      digitalWrite(ledPin_2, LOW); // ledi kapat!
+      digitalWrite(relayPin_2, LOW); // röleyi kapat!
+      role_2_bitmeSuresi = 0; // sayacı 0 la ne olur ne olmaz!
+      role_2_Acikmi = false;
+    }
+  } else {
+    sarj_2_kapat = true;
+  }
+
+
+  if(role_3_Acikmi) {
+    if(role_3_bitmeSuresi < simdi) {
+      sarj_3_kapat = true; // en son hepsini kontrol edeceğiz ona göre şarjıkapatalımmi yi değiştireceğiz!
+      digitalWrite(ledPin_3, LOW); // ledi kapat!
+      digitalWrite(relayPin_3, LOW); // röleyi kapat!
+      role_3_bitmeSuresi = 0; // sayacı 0 la ne olur ne olmaz!
+      role_3_Acikmi = false;
+    }
+  } else {
+    sarj_3_kapat = true;
+  }
+
+
+  if(role_4_Acikmi) {
+    if(role_4_bitmeSuresi < simdi) {
+      sarj_4_kapat = true; // en son hepsini kontrol edeceğiz ona göre şarjıkapatalımmi yi değiştireceğiz!
+      digitalWrite(ledPin_4, LOW); // ledi kapat!
+      digitalWrite(relayPin_4, LOW); // röleyi kapat!
+      role_4_bitmeSuresi = 0; // sayacı 0 la ne olur ne olmaz!
+      role_4_Acikmi = false;
+    }
+  } else {
+    sarj_4_kapat = true;
+  }
+
+  // tüm röleler kapanıyor mu?
+  if(sarj_1_kapat == true && sarj_2_kapat == true && sarj_3_kapat == true && sarj_4_kapat == true) {
+    sarjVarmi = false;
+    // ekranı anamenüye alalım ne olur ne olmaz!
+    ekranModu = 1;
+    modGosterildimi = false;
+    sonEkranDegisimi = millis() - ekranDonguSuresi - 1000; // her halükarda ekranın değişmesini sağlar!
+  }
+}
+
+void roleCalistir(byte roleNo) {
+  sarjVarmi = true;
+
+  if(roleNo == 1) {
+    if(role_1_Acikmi) {
+      // röle zaten açıksa süre ekle!
+      role_1_bitmeSuresi = role_1_bitmeSuresi + roleAcikKalmaSuresi;
+    } else {
+      // röle açık değilse bitme süresini ayarla. ve çalıştır!
+      role_1_bitmeSuresi = millis() + roleAcikKalmaSuresi;
+      // ledi aktifleştir!
+      digitalWrite(ledPin_1, HIGH);
+      // röleyi aç!
+      digitalWrite(relayPin_1, HIGH);
+      // rölenin açık olduğunu kaydet!
+      role_1_Acikmi = true;
+    }
+  }
+
+  if(roleNo == 2) {
+    if(role_2_Acikmi) {
+      // röle zaten açıksa süre ekle!
+      role_2_bitmeSuresi = role_2_bitmeSuresi + roleAcikKalmaSuresi;
+    } else {
+      // röle açık değilse bitme süresini ayarla. ve çalıştır!
+      role_2_bitmeSuresi = millis() + roleAcikKalmaSuresi;
+      // ledi aktifleştir!
+      digitalWrite(ledPin_2, HIGH);
+      // röleyi aç!
+      digitalWrite(relayPin_2, HIGH);
+      // rölenin açık olduğunu kaydet!
+      role_2_Acikmi = true;
+    }
+  }
+
+  if(roleNo == 3) {
+    if(role_3_Acikmi) {
+      // röle zaten açıksa süre ekle!
+      role_3_bitmeSuresi = role_3_bitmeSuresi + roleAcikKalmaSuresi;
+    } else {
+      // röle açık değilse bitme süresini ayarla. ve çalıştır!
+      role_3_bitmeSuresi = millis() + roleAcikKalmaSuresi;
+      // ledi aktifleştir!
+      digitalWrite(ledPin_3, HIGH);
+      // röleyi aç!
+      digitalWrite(relayPin_3, HIGH);
+      // rölenin açık olduğunu kaydet!
+      role_3_Acikmi = true;
+    }
+  }
+
+  if(roleNo == 4) {
+    if(role_4_Acikmi) {
+      // röle zaten açıksa süre ekle!
+      role_4_bitmeSuresi = role_4_bitmeSuresi + roleAcikKalmaSuresi;
+    } else {
+      // röle açık değilse bitme süresini ayarla. ve çalıştır!
+      role_4_bitmeSuresi = millis() + roleAcikKalmaSuresi;
+      // ledi aktifleştir!
+      digitalWrite(ledPin_4, HIGH);
+      // röleyi aç!
+      digitalWrite(relayPin_4, HIGH);
+      // rölenin açık olduğunu kaydet!
+      role_4_Acikmi = true;
+    }
+  }
+  ekranModu = 1;
+  modGosterildimi = false;
+  sonEkranDegisimi = millis() - ekranDonguSuresi - 1000; // her halükarda ekranın değişmesini sağlar!
+}
+
+void buttonKontrol() {
+  byte button_deger_1 = digitalRead(buttonPin_1);
+  byte button_deger_2 = digitalRead(buttonPin_2);
+  byte button_deger_3 = digitalRead(buttonPin_3);
+  byte button_deger_4 = digitalRead(buttonPin_4);
+
+  //delay(100);
+
+  if(button_deger_1 == HIGH) {
+    lira = lira - sarjdanDusulecekPara;
+    roleCalistir(1);
+    delay(100);
+    return; // looptan burada çıkar ve loopun başına döner! böylelikle butonlara aynı anda basılsa bile ilk sıradaki çalışır.
+  }
+
+  if(button_deger_2 == HIGH) {
+    lira = lira - sarjdanDusulecekPara;
+    roleCalistir(2);
+    delay(100);
+    return;
+  }
+
+  if(button_deger_3 == HIGH) {
+    lira = lira - sarjdanDusulecekPara;
+    roleCalistir(3);
+    delay(100);
+    return;
+  }
+
+  if(button_deger_4 == HIGH) {
+    lira = lira - sarjdanDusulecekPara;
+    roleCalistir(4);
+    delay(100);
+    return;
+  }
+}
